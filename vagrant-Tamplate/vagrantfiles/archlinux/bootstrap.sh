5bb46b485aa14430afc468fc659e61fcf705988b
Reptile8709 <89529921+Reptile8709@users.noreply.github.com>
2023-06-30 22:34:15 +0530

    Update bootstrap.sh

--- vagrant-Tamplate/vagrantfiles/archlinux/bootstrap.sh
+++ vagrant-Tamplate/vagrantfiles/archlinux/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "[TASK 1] Create venkatn user account"
-useradd -m -c "Venkat Nagappan" venkatn
-echo -e "admin\nadmin" | passwd venkatn >/dev/null 2>&1
+useradd -m -c "sam" sam
+echo -e "admin\nadmin" | passwd kadmin >/dev/null 2>&1
